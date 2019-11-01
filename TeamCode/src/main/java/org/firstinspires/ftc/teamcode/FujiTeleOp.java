4599f15e882c71825c17c72feaeafae5e6f59920
Aman <aman.shaik.us@gmail.com>
2019-11-01 19:51:33 -0400

    friday

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FujiTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FujiTeleOp.java
@@ -21,8 +21,8 @@ public class FujiTeleOp extends OpMode {
     private View relativeLayout;
     // Declare speeds.
     private static final double driveSpeed = 1;
-    private static final double hingeSpeed = 1;
-    private static final double hookSpeed = 1;
+    private static final double hingeSpeed = 0.5;
+    private static final double hookSpeed = -1;
     private static final double pinchSpeed = 1;
 
     public void init() {
