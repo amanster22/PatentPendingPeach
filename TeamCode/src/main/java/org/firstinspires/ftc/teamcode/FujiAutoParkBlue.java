fd19ed38802b9d7587a2007a6ac587e714f8535c
shaika22 <aman.shaik.us@gmail.com>
2019-11-11 17:21:54 -0500

    monday

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FujiAutoParkBlue.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="FujiAutoParkBlue", group="PatentPending")
+public final class FujiAutoParkBlue extends FujiAutoPark {
+    @Override
+    public final void runOpMode() {
+        main(false);
+    }
+}
