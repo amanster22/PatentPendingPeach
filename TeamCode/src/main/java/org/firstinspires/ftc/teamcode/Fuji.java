999636c93e415d5213fcb3b536f753dd3433a851
Yash <yash.gupta.7782@gmail.com>
2019-12-21 16:06:43 -0500

    added some new files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Fuji.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Fuji.java
@@ -1,1 +1,5 @@
 package org.firstinspires.ftc.teamcode;
+
+public final class Fuji {
+    
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FujiOp.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode;
+
+// mechanical component
+public interface FujiOp<T> {
+	// start component
+}
