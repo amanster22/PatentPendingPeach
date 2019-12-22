d6153ad2030ab6985e12f194b58b204e4c1b8fcb
Yash <yash.gupta.7782@gmail.com>
2019-12-21 19:31:31 -0500

    finished up with prelimiany fuji stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Fuji.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Fuji.java
@@ -1,20 +1,39 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
+import org.firstinspires.ftc.teamcode.hardware.general.Color;
+import org.firstinspires.ftc.teamcode.hardware.general.Distance;
 import org.firstinspires.ftc.teamcode.hardware.general.Gyro;
 import org.firstinspires.ftc.teamcode.hardware.DriveTrain;
 import org.firstinspires.ftc.teamcode.hardware.general.Motor;
+import org.firstinspires.ftc.teamcode.hardware.general.ServoM;
+import org.firstinspires.ftc.teamcode.hardware.type.Device;
 
 public final class Fuji {
     final private HardwareMap hardwaremap;
-    final private Gyro gyro;
+    final public Gyro gyro;
+    final public DriveTrain drive;
+    final public Motor rfMotor;
+    final public Motor lfMotor;
+    final public Motor rbMotor;
+    final public Motor lbMotor;
+    final public Color sensorColor;
+    final public Distance sensorDistance;
 
     //this is the constructor, pass in a hardware map to create a fuji robot instance
     public Fuji(HardwareMap map) {
         this.hardwaremap = map;
         gyro = new Gyro("imu", this.hardwaremap);
-
+        rfMotor = new Motor("rf", 1, this.hardwaremap);
+        lfMotor = new Motor("lf", 1, this.hardwaremap);
+        rbMotor = new Motor("rb", 1, this.hardwaremap);
+        lbMotor = new Motor("lb", 1, this.hardwaremap);
+        sensorColor = new Color("color", this.hardwaremap);
+        sensorDistance = new Distance("distance", this.hardwaremap);
+        drive = new DriveTrain(rfMotor, rbMotor, lfMotor, lbMotor);
     }
-
+    /*
+    Rest of this class is for high level robot functions, no logic.
+    Put logic in Teleop and autonomous
+    */
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FujiAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FujiAutonomous.java
@@ -1,7 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public abstract class FujiAutonomous extends LinearOpMode {
-
+    Fuji robot = new Fuji(hardwareMap);
 }
