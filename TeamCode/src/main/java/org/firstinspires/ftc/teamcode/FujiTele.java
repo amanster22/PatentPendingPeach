48b8dda216e1ecd3fad420f543205bf4c8f31264
Aman <aman.shaik.us@gmail.com>
2020-01-10 16:03:28 -0500

    friday

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FujiTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FujiTele.java
@@ -37,14 +37,15 @@ public final class FujiTele extends OpMode {
         forward = gamepad1.left_stick_y;
         side = gamepad1.left_stick_x;
         turn = gamepad1.right_stick_x;
-        slide = gamepad2.right_stick_y;
-        lift = gamepad2.left_stick_y;
+        lift = gamepad2.right_stick_y;
 
         //process variables and inputs
 
         if (Math.abs(forward) < 0.1) {forward = 0;}
         if (Math.abs(side) < 0.1) {side = 0;}
 
+
+        //LIFT CODE
         if (gamepad2.a) {
             // move lift up one stone
         } else if (gamepad2.b) {
@@ -53,6 +54,8 @@ public final class FujiTele extends OpMode {
             // dont do anything
         }
 
+
+        //PINCH CODE
         if (gamepad2.left_bumper) {
             // close pinch
         } else if (gamepad2.right_bumper) {
@@ -60,6 +63,38 @@ public final class FujiTele extends OpMode {
         } else {
             // do nothing, don't move pinch
         }
+
+
+        //FOUNDATION HOOK CODE
+        if (gamepad1.left_bumper) {
+            // move foundation hooks?
+        } else if (gamepad1.right_bumper) {
+            // move foundation hooks?
+        } else {
+            // move foundation hooks?
+        }
+
+
+        // FOUNDATION HOOK CODE
+        if (gamepad1.left_bumper) {
+        } // move
+        else if (gamepad1.right_bumper) {
+        } else {
+        }
+
+        //SLIDE CODE
+        if (gamepad2.dpad_up) {
+            slide = 1;
+        } // move slide forwards
+
+        else if (gamepad2.dpad_down) {
+            slide = -1;
+        }// move slide backwards
+
+        else {
+            slide = 0;
+        } // dont move slide
+
 
         robot.driveTrain.start(new DriveTrain.Vector(side * driveSpeed, forward * driveSpeed, turn * driveSpeed).speeds());
         robot.slide.start(slideSpeed * slide);
@@ -72,5 +107,7 @@ public final class FujiTele extends OpMode {
     @Override
     public final void stop() {
         robot.driveTrain.start(new DriveTrain.Vector(0, 0, 0).speeds());
+        robot.slide.start(0.0);
+        robot.lift.start(0.0);
     }
 }
